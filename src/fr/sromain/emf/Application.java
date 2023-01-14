40b1e511226bf615eed1316e6c69391f0975bc46
ROMAIN <sebastien.romain@econocom.com>
2023-01-14 17:48:55 +0100

    Afficher nombre elt model et leurs labels

--- src/fr/sromain/emf/Application.java
+++ src/fr/sromain/emf/Application.java
@@ -2,10 +2,13 @@ package fr.sromain.emf;
 
 import static java.text.MessageFormat.format;
 
+import fr.sromain.emf.model.dart.DartPackage;
+
 public class Application {
 
 	public static void main(String[] args) {
-		log("Hello {0}", "Sébastien");
+		log("Nombre d’éléments dans le modèle : {0}", DartPackage.eINSTANCE.getEClassifiers().size());
+		DartPackage.eINSTANCE.getEClassifiers().forEach(eClassifier -> log(eClassifier.getName()));
 	}
 
 	private static void log(String m, Object... p) {
